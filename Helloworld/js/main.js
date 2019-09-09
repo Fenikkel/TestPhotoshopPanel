c9d38e2b5e0023db7dfa9f3cd0e0c9921eaeefce
Pau <41298931+Fenikkel@users.noreply.github.com>
2019-09-09 15:35:06 +0200

    hello world

---
+++ Helloworld/js/main.js
@@ -0,0 +1,12 @@
+(function () {
+ 'use strict';
+
+ var csInterface = new CSInterface(); //crea una instancia de CSInterface
+
+ document.getElementById('btn_test').addEventListener('click', function () { //le añade al boton un eventlistener...
+   //con esta funcion a ejecutar
+   csInterface.evalScript('sayHello()'); //  the csInterface.evalScript method is used to send to the Photoshop JSX engine the 'sayHello()' string for evaluation
+   //realmente va al manifest y pregunta por el JSX que
+  });
+
+}());
---
+++ Helloworld/jsx/photoshop.jsx
@@ -0,0 +1,6 @@
+
+//todas las funciones seran propiedad de CSInterface?
+
+function sayHello(){ // esta funcion sera llamada desde CSInterface que hara de mediador entre el boton del panel (tu) y las caracteristicas de CEP (photoshop)
+  alert("Hello from ExtendScript");
+}
