5da8549d55b51bffabdc417759578e4b525c5d36
Pau <41298931+Fenikkel@users.noreply.github.com>
2019-10-15 09:59:44 +0200

    Substract on apply changes DONE

--- HeightmapEditor/HeightmapEditor/ApplyChanges/js/main.js
+++ HeightmapEditor/HeightmapEditor/ApplyChanges/js/main.js
@@ -18,7 +18,9 @@ console.log("Apply changes started");
  var gBCanvas = document.createElement("CANVAS");//document.getElementById("cnvsBase");//document.createElement("CANVAS");
  var gSCanvas = document.createElement('canvas');
 
-
+ var gContext = gCanvas.getContext("2d");
+ var gBContext = gBCanvas.getContext("2d");
+ var gSContext = gSCanvas.getContext("2d");
 
 
  function init() {
@@ -27,21 +29,102 @@ console.log("Apply changes started");
 
      csInterface.addEventListener("com.fenikkel.event.applyChanges", getEventData); //obtenemos toda la informacion necesaria para pintar el canvas y procesarlo
 
+
+     //SI NO FUNCIONA, SUBIR TIEMPOS
      setTimeout(function () {
        //alert(gBaseInfo.height);
        //alert(gSubstracterInfo.width);
        processCanvas();
+       //substract(); //no porque  process canvas trabaja paralelamente y aun no estan cargados los canvas
+       setTimeout(function () {
+         substract();
+       }, 500)
      }, 500)
+
      setTimeout(function () {
        alert("cerramos")
        csInterface.closeExtension();
-     }, 4000)
+     }, 6000)
 
    } catch(e) {
      alert(e.line + " - " + e);
    }
 
  }
+
+function substract(){
+
+
+  gContext.fillStyle = "#111111"; //Pinta de VERDE PERO LUEGO LO PINTA TODO DE BLANCO...
+
+  gContext.fillRect(0, 0, gCanvas.width, gCanvas.height); //relleno todo el canvas de amarillo
+
+  gContext = gCanvas.getContext("2d");
+  gBContext = gBCanvas.getContext("2d");
+  gSContext = gSCanvas.getContext("2d");
+
+  //NO OBTIENEN LA DATA? METER CADA UNO EN EL RESULTADO FINAL A VER
+  var baseData = gBContext.getImageData(0, 0, gBCanvas.width, gBCanvas.height);
+  //alert(baseData);
+  var substracterData = gSContext.getImageData(0, 0,  gSCanvas.width, gSCanvas.height);
+  var finalData = gContext.getImageData(0, 0,  gCanvas.width, gCanvas.height);
+
+  var dataIdx = 0;
+
+  var red = 0;
+  var green = 0;
+  var blue = 0;
+  var alpha = 0;
+
+  //alert(baseData.data.length);
+  while (dataIdx < baseData.data.length) {
+
+      red = baseData.data[dataIdx] - substracterData.data[dataIdx]; //Red
+      green = baseData.data[dataIdx+1] - substracterData.data[dataIdx+1]; //Green
+      blue = baseData.data[dataIdx+2] - substracterData.data[dataIdx+2]; //Blue
+
+      if(baseData.data[dataIdx+3] > substracterData.data[dataIdx+3]){
+
+        alpha = substracterData.data[dataIdx+3]; //alpha
+
+      }
+      else{
+        alpha = baseData.data[dataIdx+3];
+      }
+
+
+      if(red < 0){
+        finalData.data[dataIdx] = 0;
+      }
+      else{
+        finalData.data[dataIdx] = red;
+      }
+      if(green < 0){
+        finalData.data[dataIdx+1] = 0;
+      }
+      else{
+        finalData.data[dataIdx+1] = green;
+      }
+      if(blue < 0){
+        finalData.data[dataIdx+2] = 0;
+      }
+      else{
+        finalData.data[dataIdx+2] = blue;
+      }
+
+      finalData.data[dataIdx+3] = alpha;
+
+      dataIdx = dataIdx + 4; //pintamos de 4 pixeles en 4
+
+  }
+
+  gContext.putImageData(finalData, 0, 0); //finalData
+  //gContext.putImageData(finalData, 0, 0); //finalData
+  //gBContext.putImageData(substracterData, 0, 0); //finalData
+
+
+
+}
 
 function processCanvas(){
 
@@ -51,40 +134,34 @@ function processCanvas(){
   gBCanvas.width = parseFloat(gBaseInfo.width);
   gBCanvas.height = parseFloat(gBaseInfo.height);
 
+
+
   gSCanvas.width = parseFloat(gBaseInfo.width);
   gSCanvas.height = parseFloat(gBaseInfo.height);
-
-
-  var context = gCanvas.getContext("2d");
-  var bContext = gBCanvas.getContext("2d");
-  var sContext = gSCanvas.getContext("2d");
-
-  //bContext.fillStyle = "#FFFF00";
-  //bContext.fillRect(20, 20, 150, 100);
 
   document.body.appendChild(gBCanvas);
   document.body.appendChild(gSCanvas);
   document.body.appendChild(gCanvas);
-  context.fillStyle = "#FFFF00";
-  context.fillRect(0, 0, 500, 500);
+  gContext.fillStyle = "#FFFF00";
+  gContext.fillRect(0, 0, 500, 500);
 
 
   gImageBaseData.onload = function () {
-    bContext.drawImage(gImageBaseData, 0, 0, gBCanvas.width, gBCanvas.height);
+    gBContext.drawImage(gImageBaseData, 0, 0, gBCanvas.width, gBCanvas.height);
     //updatePreview();
   };
   gImageBaseData.src = gBaseInfo.url;
 
 
   gImageSubstracterData.onload = function () {
-    sContext.drawImage(gImageSubstracterData, 0, 0, gSCanvas.width, gSCanvas.height);
-    //updatePreview();
+    gSContext.drawImage(gImageSubstracterData, 0, 0, gSCanvas.width, gSCanvas.height);
+
   };
   gImageSubstracterData.src = gSubstracterInfo.url;
 
+  //substract();
+
   //alert("base: " + gBaseInfo.url+ "\n" + " substracter" +gSubstracterInfo.url);
-
-
 }
 
 
